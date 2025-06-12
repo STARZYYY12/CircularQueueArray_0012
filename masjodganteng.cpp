d00fd206a7534fd9cc85aeff8c22f590df6f0554
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-12 11:00:47 +0700

    jika front < rear

--- masjodganteng.cpp
+++ masjodganteng.cpp
@@ -84,4 +84,15 @@ public:
             }
             cout << endl;
         }
+        else {
+        //jika FRONT_position < REAR_position
+        // iterasi dari FRONT HINGGA REAR
+
+        while (FRONT_position <= max -1) {
+            cout << queue_array[FRONT_position] << "  ";
+            FRONT_position++;
+        }
+
+        FRONT_position = 0;
         
+       
