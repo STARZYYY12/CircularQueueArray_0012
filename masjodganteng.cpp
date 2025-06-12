9c1864acdf81382ef526609f6c4a0a34ac081e8c
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-12 10:59:54 +0700

    jika front <= Rear

--- masjodganteng.cpp
+++ masjodganteng.cpp
@@ -75,3 +75,13 @@ public:
 
         cout << "\nElement in the queue are...\n";
 
+        //jika FRONT_position <= REAR_position
+        // iterasi dari FRONT HINGGA REAR
+        if (FRONT_position <= REAR_position){
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        
