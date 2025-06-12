f8a839cf4ecfe23a81186a13af72f465638c143d
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-12 10:11:41 +0700

    member class

--- masjodganteng.cpp
+++ masjodganteng.cpp
@@ -2,3 +2,15 @@
 using namespace std;
 
 //CircularQueueArray_4NIMBelakang
+
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+
+public:
+    Queues(){
+        FRONT =-1;
+        REAR = -1;
+    }
+
+   
