1a8e6362419b7350dec07ec287007caf21824793
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-06-12 10:26:31 +0700

    ngecek jika rear berada diakhir kembalikan ke awal

--- masjodganteng.cpp
+++ masjodganteng.cpp
@@ -26,4 +26,16 @@ public:
 
           }
         
-          
+          else {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+            REAR = 0;
+            else 
+            REAR = REAR + 1;
+
+          }
+          queue_array[REAR] = num;
+
+    }
+
+    
